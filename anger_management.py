94d4aaed46fb2992da76d2bdad09aff4ccfaef99
Alex <alexander.g.cooper@gmail.com>
2016-12-29 15:37:33 -0500

    Update anger_management.py

--- anger_management.py
+++ anger_management.py
@@ -5,10 +5,10 @@ import time
 import sys
 
 def delay_print(s):
-  for c in s:
-    sys.stdout.write( '%s' % c )
-    sys.stdout.flush()
-    time.sleep(0.10)
+    for c in s:
+        sys.stdout.write( '%s' % c )
+        sys.stdout.flush()
+        time.sleep(0.10)
 
 def main():
     count = input("On a scale of 1 to 10, how mad are you? ")
