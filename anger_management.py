f22e3e40dbfebf1bf504b7f4ce0096c74352c413
Alex <alexander.g.cooper@gmail.com>
2016-12-29 14:51:18 -0500

    Update anger_management.py

--- anger_management.py
+++ anger_management.py
@@ -10,10 +10,11 @@ def delay_print(s):
     sys.stdout.flush()
     time.sleep(0.10)
 
-count = input("On a scale of 1 to 10, how mad are you? ")
+def main():
+    count = input("On a scale of 1 to 10, how mad are you? ")
+    while count !=0:
+        delay_print("\n" + "      *       *" + "\n" + "    *   *   *   *" + "\n" + "   *      *     *" + "\n" + "    *          *" + "\n" + "     *        *" + "\n" + "      *      *" + "\n" + "       *    *" + "\n" + "        *  *" + "\n" + "         **" + "\n" + "\n")
+        count -= 1
 
-while count !=0:
-  delay_print("\n" + "      *       *" + "\n" + "    *   *   *   *" + "\n" + "   *      *     *" + "\n" + "    *          *" + "\n" + "     *        *" + "\n" + "      *      *" + "\n" + "       *    *" + "\n" + "        *  *" + "\n" + "         **" + "\n" + "\n")
-  count -= 1
-
-sys.exit()
+if __name__=="__main__":
+    sys.exit(main())
